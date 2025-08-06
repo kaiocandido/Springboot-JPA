10eb25885560d9a528687f562f00ac70121b8f8a
adm <kaio.candf@gmail.com>
2025-08-06 16:47:12 -0300

    ajustado JPA

---
+++ src/test/java/Aula_JPA/Aula_JPA/AulaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Aula_JPA.Aula_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
